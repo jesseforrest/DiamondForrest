78035dcc74a6ecb8ca14ef7e9b28d105e2466a31
Jesse Forrest <jesseforrest@gmail.com>
2013-02-13 13:37:36 -0800

    Added database constants to config files.

--- application/configurations/configLocal.php
+++ application/configurations/configLocal.php
@@ -43,3 +43,38 @@ define('IS_STAGING', false);
  * @var boolean
  */
 define('IS_PRODUCTION', false);
+
+/**
+ * The IP address of the MySQL database server
+ *
+ * @var string
+ */
+define('DB_SERVER', '127.0.0.1');
+
+/**
+ * The username of the user that can connect to the MySQL database server
+ *
+ * @var string
+ */
+define('DB_USER', 'username');
+
+/**
+ * The password of the user that can connect to the MySQL database server
+ *
+ * @var string
+ */
+define('DB_PASS', 'password');
+
+/**
+ * The name of the database on the MySQL database server
+ *
+ * @var string
+ */
+define('DB_NAME', 'database_name');
+
+/**
+ * The port number that the MySQL database server is listening on
+ *
+ * @var string
+ */
+define('DB_PORT', '3306');
